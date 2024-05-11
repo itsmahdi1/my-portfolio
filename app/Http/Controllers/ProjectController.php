df9240be057c8b10ba6027eb76fe6c63a10bc74c
Mahdi <e.mahdimiftah@gmail.com>
2024-05-11 15:06:30 +0100

    Create resources pages

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ProjectController extends Controller
 {
@@ -11,7 +12,7 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        return 'This is my projects';
+        return Inertia::render("Projects/Index");
     }
 
     /**
