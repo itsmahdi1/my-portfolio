6957eca7b54188f7fcf5220772c4d8897cb3a79a
Mahdi <e.mahdimiftah@gmail.com>
2024-05-12 20:54:37 +0100

    create skill from

--- resources/js/Pages/Skills/Create.vue
+++ resources/js/Pages/Skills/Create.vue
@@ -9,7 +9,21 @@
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                Form
+                <form @submit.prevent="form.post('/login')">
+                    <!-- email -->
+                    <input type="text" v-model="form.name" />
+                    <div v-if="form.errors.name">{{ form.errors.name }}</div>
+                    <!-- password -->
+                    <input type="file" @input="form.image = $event.target.files[0]" />
+                    <div v-if="form.errors.image">
+                        {{ form.errors.image }}
+                    </div>
+
+                    <!-- submit -->
+                    <button type="submit" :disabled="form.processing">
+                        Store
+                    </button>
+                </form>
             </div>
         </div>
     </AuthenticatedLayout>
@@ -17,11 +31,20 @@
 
 <script>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
-import { Head } from "@inertiajs/vue3";
+import { Head, useForm } from "@inertiajs/vue3";
 export default {
     components: {
         Head,
+        useForm,
         AuthenticatedLayout,
+    },
+    setup() {
+        const form = useForm({
+            name: '',
+            image: null,
+        });
+
+        return { form };
     },
 };
 </script>
