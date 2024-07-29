71a4d75691b768ed62812a6ba07f6c9c7b8e8380
Mahdi <e.mahdimiftah@gmail.com>
2024-07-29 15:47:15 +0100

    fix css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,13 @@ export default {
         "./storage/framework/views/*.php",
         "./resources/views/**/*.blade.php",
         "./resources/js/**/*.vue",
+        "./resources/css/**/*.css" // Include CSS files
     ],
 
     theme: {
         fontFamily: {
             primary: "Playfair Display",
-            body: "work Sans",
+            body: "Work Sans",
         },
         container: {
             padding: {
@@ -25,25 +26,25 @@ export default {
         extend: {
             fontFamily: {
                 sans: ["Nunito", ...defaultTheme.fontFamily.sans],
-                colors: {
-                    "light-primary": "#FAF1E6",
-                    "light-secondary": "#FDFAF6",
-                    "light-tail-100": "#A9DED2",
-                    "light-tail-500": "#54BAB9",
-                    "dark-primary": "#050402",
-                    "dark-secondary": "#1C1D24",
-                    "dark-navy-100": "#07567D",
-                    "dark-navy-500": "#292D42",
-                    accent: {
-                        DEFAULT: "#ac6b34",
-                        hover: "#925a2b",
-                    },
-                    paragraph: "#878e99",
-                    blue: colors.blue,
-                    indigo: colors.indigo,
-                    green: colors.green,
-                    red: colors.red,
+            },
+            colors: {
+                "light-primary": "#FAF1E6",
+                "light-secondary": "#FDFAF6",
+                "light-tail-100": "#A9DED2",
+                "light-tail-500": "#54BAB9",
+                "dark-primary": "#050402",
+                "dark-secondary": "#1C1D24",
+                "dark-navy-100": "#07567D",
+                "dark-navy-500": "#292D42",
+                accent: {
+                    DEFAULT: "#ac6b34",
+                    hover: "#925a2b",
                 },
+                paragraph: "#878e99",
+                blue: colors.blue,
+                indigo: colors.indigo,
+                green: colors.green,
+                red: colors.red,
             },
         },
     },
